aace939b63f40d6b603b0039b1a1cd844ded2448
007jedgar <007j.edgar@gmail.com>
2025-12-16 10:06:23 -0600

    fix: credit card detection

--- wxt.config.ts
+++ wxt.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     name: 'PasteProof',
     description:
       'Your pasteboard bodyguard. Prevents you from pasting sensitive data into the wrong fields.',
-    version: '0.1.9',
+    version: '0.2.0',
     permissions: [
       'storage', // For storing user settings
       'activeTab', // Required for some interactions
