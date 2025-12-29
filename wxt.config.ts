c642c861cfd7801a689e1fcded095226a1bff009
007jedgar <007j.edgar@gmail.com>
2025-12-29 10:51:10 -0600

    chore: bump version to 0.2.2 and implement text replacement for anonymization in content script

--- wxt.config.ts
+++ wxt.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     name: 'PasteProof',
     description:
       'Your pasteboard bodyguard. Prevents you from pasting sensitive data into the wrong fields.',
-    version: '0.2.1',
+    version: '0.2.2',
     permissions: [
       'storage', // For storing user settings
       'activeTab', // Required for some interactions
