d57f751ade982db67ab14f707ff01a3ad4efd327
007jedgar <007j.edgar@gmail.com>
2026-03-06 08:51:00 -0600

    build: bump v

--- wxt.config.ts
+++ wxt.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     name: 'PasteProof',
     description:
       'Your pasteboard bodyguard. Prevents you from pasting sensitive data into the wrong fields.',
-    version: '0.2.5',
+    version: '0.2.6',
     permissions: [
       'storage', // For storing user settings
       'activeTab', // Required for some interactions
